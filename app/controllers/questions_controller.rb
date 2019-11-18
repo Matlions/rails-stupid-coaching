3dbd8c589b96e50506fcef6bf0000928f85b8316
Matlions <matthieurpeyronnet@gmail.com>
2019-11-18 10:59:15 +0100

    stupid coaching fixed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,20 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
   def answer
+    @coach_answer = ''
     @question_asked = params[:question]
 
-    if @question_asked.to_s.include? "?"
-      @coach_answer = "Silly question, get dressed and go to work!"
-
-    elsif @question_asked == "I am going to work right now!"
-      @coach_answer = " Great!"
-    else # your_message.class == String
+    if @question_asked == 'I am going to work'
+      @coach_answer = 'Great!'
+    elsif @question_asked.last == '?'
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    else
       @coach_answer = "I don't care, get dressed and go to work!"
     end
-    # @question
   end
 end
+#   if @question_asked.to_s.include? "?"
 # question c'est la clee
 # reponse c'est la valeur
